96d7daba2b7d642f43618e02eca9ce0f385b674f
MontebellerZ <vyatrhjar@gmail.com>
2026-04-19 11:23:21 -0300

    fix: corrigir navegação inicial para redirecionar corretamente para a tela de turmas

--- front/src/router/index.tsx
+++ front/src/router/index.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Navigate to={"turmas"} />,
+        element: <Navigate to={"turmas"} replace/>,
       },
       {
         path: "turmas",
